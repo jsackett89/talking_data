e461b635495cf9bf2112c51f23c02bdd2165c04f
jsackett <jsackett89@gmail.com>
2016-07-11 23:03:01 -0500

    Reads files into dictionary

--- talking_data.py
+++ talking_data.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 11 20:15:45 2016
+Created on Mon Jul 11 22:35:30 2016
 
 @author: sackettj
 """
 
+# Load necessary packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import glob
+
+# Create list of csv filepaths in working project directory
+cwd = os.getcwd()
+filePaths = [cwd + "\\" + f for f in glob.glob("*.csv")]
+# File names
+fileNames = ['app_labels', 'events', 'ga_test', 'ga_train', 'labels', 'pbdm', 'sample_sub']
+# Create tuples for dictionary comprehension
+fileKeyVal = zip(fileNames, filePaths)
+# Create dictionary of files
+fileDict = {name : pd.read_csv(file) for name, file in  fileKeyVal}
+
+# Explore files
 
